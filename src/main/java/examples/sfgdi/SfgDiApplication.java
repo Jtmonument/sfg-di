51d8d1a5cfe2479c04f1dc836f3e99ef140986b7
jonta <jontaylor091@gmail.com>
2022-06-04 17:48:22 -0500

    Exemplify Spring Dependency Injection

--- src/main/java/examples/sfgdi/SfgDiApplication.java
+++ src/main/java/examples/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package examples.sfgdi;
 
+import examples.sfgdi.controllers.ConstructorInjectedController;
 import examples.sfgdi.controllers.MyController;
+import examples.sfgdi.controllers.PropertyInjectedController;
+import examples.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +15,21 @@ public class SfgDiApplication {
         ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
         MyController controller = (MyController) context.getBean("myController");
         System.out.println(controller.helloWorld());
+
+        /*Property Dependency Injection*/
+        PropertyInjectedController propController = (PropertyInjectedController)
+                context.getBean("propertyInjectedController");
+        System.out.println(propController.greet());
+
+        /*Setter Dependency Injection*/
+        SetterInjectedController setController = (SetterInjectedController)
+                context.getBean("setterInjectedController");
+        System.out.println(setController.greet());
+
+        /*Constructor Dependency Injection*/
+        ConstructorInjectedController constructedController = (ConstructorInjectedController)
+                context.getBean("constructorInjectedController");
+        System.out.println(constructedController.greet());
     }
 
 }
