a7635de7cc31cda506ed18f47c96b5fd49c397b4
jonta <jontaylor091@gmail.com>
2022-06-04 17:58:12 -0500

    Inject Greeting Service

--- src/main/java/examples/sfgdi/controllers/MyController.java
+++ src/main/java/examples/sfgdi/controllers/MyController.java
@@ -1,9 +1,21 @@
 package examples.sfgdi.controllers;
 
+import examples.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService service;
+
+    public MyController(GreetingService service) {
+        this.service = service;
+    }
+
+    public String greet() {
+        return service.getGreeting();
+    }
+
     public String helloWorld() {
         System.out.println("Hello World!");
         return "Printed successfully!";
