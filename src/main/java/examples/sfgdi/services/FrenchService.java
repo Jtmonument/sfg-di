e69fd09be5a66b66ad264efb5d965f924677e2e9
jonta <jontaylor091@gmail.com>
2022-06-04 18:12:24 -0500

    Create French Service

---
+++ src/main/java/examples/sfgdi/services/FrenchService.java
@@ -0,0 +1,13 @@
+package examples.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("FR")
+@Service("I18n")
+public class FrenchService implements GreetingService {
+    @Override
+    public String getGreeting() {
+        return "Bon matin!";
+    }
+}
